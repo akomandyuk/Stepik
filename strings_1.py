e280e4cdc5f6d0db488ffdbd8cd0bc69955e0fae
akomandyuk <artyomkomandyuk@gmail.com>
2021-11-23 21:03:49 +0300

    comments added in a strings task 1/2

--- strings_1.py
+++ strings_1.py
@@ -1,4 +1,6 @@
+# You write a genome sequence
 genome = input('')
+# We make all the letters in lowercase, so the code will work no matter what the input was
 genome = genome.lower()
 
 # symbols 'c' and 'g'
@@ -6,11 +8,13 @@ count = 0
 # all symbols in a string
 length = 0
 
+# for all the letters in 'genome' we check how many 'c' and 'g' are in it
 for i in genome:
     if i == 'c' or i == 'g':
         count += 1
     length += 1
 
-#print(count)
-#print(length)
+print(count)
+print(length)
+# We print the percentage of 'c' and 'g' in the genome string
 print(count / length * 100)
