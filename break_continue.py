5f8fc786279394106e9511e635b292d4071bf092
akomandyuk <artyomkomandyuk@gmail.com>
2021-11-26 23:43:15 +0300

    Comments added to break/continue task

--- break_continue.py
+++ break_continue.py
@@ -1,9 +1,13 @@
 a = 0
 
+# If a number > 100 we stop reading numbers
 while a <=100:
-    a = int(input())
+    # We type a number
+    a = int(input('Type your number: '))
+    # If a number < 10, we skip it
     if a < 10:
         continue
+    # If a number > 100, we end the cycle
     if a > 100:
         break
     print(a)
