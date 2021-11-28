03f4c78bb008246689d0d653db01771711ef961d
akomandyuk <artyomkomandyuk@gmail.com>
2021-11-28 21:59:52 +0300

    Comments added to while cycle task 1

--- while_1.py
+++ while_1.py
@@ -1,6 +1,12 @@
+# We set a sum variable
 sum = 0
-a = int(input())
+# We set a variable where we type numbers
+a = int(input('Type your number: '))
+# The cycle works until we type '0'
 while a != 0:
+    # In case we don't write '0' we add our number to 'sum' variable
     sum += a
-    a = int(input())
+    # Then we type our number again
+    a = int(input('Type your number: '))
+# When the cycle is over we print the total sum
 print(sum)
