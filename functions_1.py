7e15953b028f108b4acd9d8c042118b9c85f8111
akomandyuk <artyomkomandyuk@gmail.com>
2021-12-08 11:58:25 +0300

    Comments added for functions task 1/2

--- functions_1.py
+++ functions_1.py
@@ -1,3 +1,6 @@
+# Create a function
+# Depending on the number the function will give different outputs
+# which are written below
 def f(x):
     if x <= -2:
         return 1 - ((x + 2) ** 2)
@@ -6,6 +9,6 @@ def f(x):
     else:
         return ((x - 2) ** 2) + 1
 
-
+# As an example we create a variable 'a' with number 1
 a = f(1)
 print(a)
